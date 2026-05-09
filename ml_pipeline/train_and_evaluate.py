d854d57e3a0bd61b6658c28cdbd98ff6d1028d13
Karan Sarao <karan.sarao@snowflake.com>
2026-05-08 22:27:22 -0400

    Changed basics

--- ml_pipeline/train_and_evaluate.py
+++ ml_pipeline/train_and_evaluate.py
@@ -32,13 +32,13 @@ def main():
     X = pdf[feature_cols].fillna(0)
     y = pdf["READMITTED_30_DAY"]
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42
+        X, y, test_size=0.15, random_state=42
     )
 
     # Train XGBoost
     t0 = time.time()
     model = XGBClassifier(
-        n_estimators=250, max_depth=5, learning_rate=0.1, random_state=42
+        n_estimators=300, max_depth=5, learning_rate=0.1, random_state=42
     )
     model.fit(X_train, y_train)
     train_time = round(time.time() - t0, 2)
